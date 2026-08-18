c3cc6f274c63422efc7ed12ac01cb489891cfa30
akash.patel <akash.patel@skykingindia.com>
2026-08-19 00:56:46 +0530

    arena

---
+++ src/Application/TheGameVoice.Application/Interfaces/Persistence/IDashboardService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGameVoice.Application.Interfaces.Persistence
+{
+    internal class IDashboardService
+    {
+    }
+}
--- src/Web/TheGameVoice.Web/Areas/Admin/ViewModels/Dashboard/DashboardViewModel.cs
+++ src/Web/TheGameVoice.Web/Areas/Admin/ViewModels/Dashboard/DashboardViewModel.cs
@@ -2,27 +2,19 @@
 
 public class DashboardViewModel
 {
-    public int DraftArticlesCount { get; set; }
+    public int TotalArticles { get; set; }
 
-    public int ReviewPendingCount { get; set; }
+    public int PublishedArticles { get; set; }
 
-    public int PublishedArticlesCount { get; set; }
+    public int ScheduledArticles { get; set; }
 
-    public int ArchivedArticlesCount { get; set; }
+    public int PendingReviewArticles { get; set; }
 
-    public int UsersCount { get; set; }
+    public int DraftArticles { get; set; }
 
-    public int MediaCount { get; set; }
+    public int RejectedArticles { get; set; }
 
-    public int CategoriesCount { get; set; }
+    public int ArchivedArticles { get; set; }
 
-    public int TagsCount { get; set; }
-
-    public List<DashboardArticleItemViewModel>
-        RecentArticles
-    {
-        get;
-        set;
-    }
-    = new();
+    public long TotalViews { get; set; }
 }
